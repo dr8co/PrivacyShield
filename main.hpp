88feb808ef5cfa8717ac8eda0111e47be9a8291d
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-06-10 09:20:41 +0300

    Update the main header with function prototypes

--- main.hpp
+++ main.hpp
@@ -1,5 +1,12 @@
 #ifndef PRIVACY_SHIELD_MAIN_HPP
 #define PRIVACY_SHIELD_MAIN_HPP
 
+#include <string>
+
+bool encryptFile(const std::string &inputFile, const std::string &outputFile, const std::string &password);
+
+bool findDuplicates(const std::string &directoryPath);
+
+bool decryptFile(const std::string &inputFile, const std::string &outputFile, const std::string &password);
 
 #endif //PRIVACY_SHIELD_MAIN_HPP
