bfb6d9861aff1352067827693f5536403e10720e
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-06-13 13:23:57 +0300

    Overwrite a file with random bytes

---
+++ shredFiles.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+
+void overwriteRandom(const std::string& filename) {
+    std::ofstream file(filename, std::ios::binary | std::ios::in);
+    if (!file) {
+        std::cerr << "Failed to open the file: " << filename << std::endl;
+        return;
+    }
+
+    // Get the file size
+    file.seekp(0, std::ios::end);
+    std::streamoff fileSize = file.tellp();
+    file.seekp(0, std::ios::beg);
+
+    // Create a random device for generating secure random numbers
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<uint8_t> dist(0, 255);
+
+    // Overwrite the file with random data
+    for (std::streamoff pos = 0; pos < fileSize; ++pos) {
+        uint8_t randomByte = dist(gen);
+        file.write(reinterpret_cast<char*>(&randomByte), sizeof(uint8_t));
+    }
+
+    file.close();
+}
