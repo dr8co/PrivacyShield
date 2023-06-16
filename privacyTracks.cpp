c11f19a300b7225be608e721283b00bba58e2d6e
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-06-16 13:57:31 +0300

    Detect installed browsers

---
+++ privacyTracks.cpp
@@ -0,0 +1,26 @@
+#include <filesystem>
+#include <iostream>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> detectBrowsers() {
+    std::vector<std::string> browsers;
+
+    // List of common browser executable names
+    std::vector<std::string> browserExecutables = {
+            "firefox",
+            "google-chrome",
+            "chromium-browser",
+            "opera",
+    };
+
+    // Search for each browser executable in the PATH environment variable
+    for (const auto &browser: browserExecutables) {
+        if (fs::exists(fs::path("/usr/bin/" + browser))) {
+            browsers.push_back(browser);
+        }
+    }
+
+    return browsers;
+}
