85c86090a4efe8cb9a7e28e96cf72ffb6dc1d9e9
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-06-21 15:53:57 +0300

    Add a prompt for sensitive info and enable editing

--- passwords.cpp
+++ passwords.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
+#include <readline/readline.h>
 
 /**
  * @brief reads sensitive input from a terminal without echoing them.
+ * @param prompt the prompt to display.
  * @return a string of the information read.
  */
-std::string getSensitiveInfo() {
+std::string getSensitiveInfo(const std::string &prompt = "") {
     std::string password;
-    termios oldSettings, newSettings;
+    char *tmp;
+    termios oldSettings{}, newSettings{};
 
     // Turn off terminal echoing
     tcgetattr(STDIN_FILENO, &oldSettings);
@@ -17,7 +20,9 @@ std::string getSensitiveInfo() {
     tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
 
     // Read password from input
-    std::getline(std::cin, password);
+    tmp = readline(prompt.c_str());
+    password = std::string(tmp);
+    std::free(tmp);
 
     // Restore terminal settings
     tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
