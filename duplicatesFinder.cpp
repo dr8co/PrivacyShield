19d9bfe4a879de1c8670644eb5a81cf96b9f4edd
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-07-09 19:37:43 +0300

    Use 512-bit BLAKE2b hash to identify duplicate files
    This change reduces the chances of 2 or more files with different content producing the same hash value.
    Also, the hash is base64-encoded since the output of the hashing function is binary data which may have embedded NULs ('\0'), so passing them around as strings might be problematic.

--- duplicatesFinder.cpp
+++ duplicatesFinder.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <unordered_map>
-#include <vector>
 #include <filesystem>
 #include <sodium.h>
 #include <thread>
+#include "main.hpp"
 
 namespace fs = std::filesystem;
 
-// Structure to store file information.
+/**
+ * @brief Represents a file by its canonical path and hash.
+ * @
+ */
 struct FileInfo {
     std::string path; // path to the file.
     std::string hash; // Blake2b hash of the file.
 };
 
 /**
- * @brief Calculates the Blake2b hash of a file.
+ * @brief Calculates the BLAKE2b hash of a file.
  * @param filePath path to the file.
  * @return a string of the hash of the file.
  */
@@ -29,10 +31,12 @@ std::string calculateBlake2b(const std::string &filePath) {
     std::vector<char> buffer(bufferSize);
 
     crypto_generichash_blake2b_state state;
-    if (crypto_generichash_blake2b_init(&state, nullptr, 0, crypto_generichash_blake2b_BYTES) != 0)
+
+    // Initialize the hashing process with the state, and set the output length to 512 bits (64 bytes)
+    if (crypto_generichash_blake2b_init(&state, nullptr, 0, crypto_generichash_BYTES_MAX) != 0)
         throw std::runtime_error("Failed to initialize Blake2b hashing.");
 
-
+    // Hash the file in chunks of 4kB
     while (file.read(buffer.data(), bufferSize)) {
         if (crypto_generichash_blake2b_update(&state,
                                               reinterpret_cast<const unsigned char *>(buffer.data()),
@@ -40,22 +44,26 @@ std::string calculateBlake2b(const std::string &filePath) {
             throw std::runtime_error("Failed to calculate Blake2b hash.");
     }
 
+    // Hash the last chunk of data
     size_t remainingBytes = file.gcount();
     if (crypto_generichash_blake2b_update(&state,
                                           reinterpret_cast<const unsigned char *>(buffer.data()),
                                           remainingBytes) != 0)
         throw std::runtime_error("Failed to calculate Blake2b hash.");
 
-
-    unsigned char hash[crypto_generichash_blake2b_BYTES];
-    if (crypto_generichash_blake2b_final(&state, hash, crypto_generichash_blake2b_BYTES) != 0)
+    // Finalize the hash calculation
+    std::vector<unsigned char> hash(crypto_generichash_BYTES_MAX);
+    if (crypto_generichash_blake2b_final(&state, hash.data(), crypto_generichash_BYTES_MAX) != 0)
         throw std::runtime_error("Failed to finalize Blake2b hash calculation.");
 
-    std::string blake2bHash(reinterpret_cast<const char *>(hash), crypto_generichash_blake2b_BYTES);
+    // Since the hash is raw bytes, Base64-encode it for string handling
+    std::string blake2bHash(base64Encode(hash));
 
     file.close();
     return blake2bHash;
 }
+
+// TODO: For 32-bit systems, use 32-bit BLAKE2s instead of BLAKE2b
 
 /**
  * @brief recursively traverses a directory and collects file information.
@@ -64,12 +72,15 @@ std::string calculateBlake2b(const std::string &filePath) {
  */
 void traverseDirectory(const std::string &directoryPath, std::vector<FileInfo> &files) {
     for (const auto &entry: fs::recursive_directory_iterator(directoryPath)) {
-        if (entry.is_regular_file()) {
+        // process only regular files
+        if (entry.is_regular_file()) [[likely]]{
             FileInfo fileInfo;
+
+            // Update file details
             fileInfo.path = entry.path().string();
             fileInfo.hash = "";  // Hash will be calculated later
             files.push_back(fileInfo);
-        } else if (!entry.is_directory())
+        } else if (!entry.is_directory()) // skip directories
             std::cout << "Not processing: " << entry << ". Not a regular file." << std::endl;
     }
 }
@@ -104,7 +115,7 @@ size_t findDuplicates(const std::string &directoryPath) {
     std::vector<FileInfo> files;
     traverseDirectory(directoryPath, files);
 
-    // Number of threads to use (Simple C11 threads for now)
+    // Number of threads to use (Simple C++11 threads for now)
     const unsigned int numThreads = std::thread::hardware_concurrency();
 
     // Divide files among threads and calculate hashes in parallel
