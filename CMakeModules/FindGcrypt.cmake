ad1e544920dbd2467a585ff883872dbcda855f6d
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-09-05 12:18:43 +0300

    Update CMake scripts

--- CMakeModules/FindGcrypt.cmake
+++ CMakeModules/FindGcrypt.cmake
@@ -1,82 +1,46 @@
+# A CMake module to find a local installation of Gcrypt
 
+# This module sets the following variables:
+#   GCRYPT_FOUND       - True if Gcrypt is found
+#   GCRYPT_INCLUDE_DIR - Include directories for Gcrypt
+#   GCRYPT_LIBRARIES   - Linker flags for Gcrypt
+#   GCRYPT_VERSION     - Version of Gcrypt
 
-# - Try to find the Gcrypt library
-# Once run this will define
-#
-#  LIBGCRYPT_FOUND - set if the system has the gcrypt library
-#  LIBGCRYPT_CFLAGS - the required gcrypt compilation flags
-#  LIBGCRYPT_LIBRARIES - the linker libraries needed to use the gcrypt library
-#
-# libgcrypt is moving to pkg-config, but earlier version don't have it
-# 
-# Copyright (c) 2006 Brad Hards <bradh@kde.org>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 
-# 1. Redistributions of source code must retain the copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products 
-#    derived from this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# This module also provides the imported target Gcrypt::Gcrypt
 
-if(NOT LibGcrypt_FIND_VERSION)
-    set(LibGcrypt_FIND_VERSION "1.10.0")
-endif()
+# Find the pkg-config package for Gcrypt
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(GCRYPT REQUIRED libgcrypt)
 
-#search in typical paths for libgcrypt-config
-FIND_PROGRAM(LIBGCRYPTCONFIG_EXECUTABLE NAMES libgcrypt-config)
+# Set the Gcrypt variables
+set(GCRYPT_FOUND TRUE)
+set(GCRYPT_INCLUDE_DIR ${GCRYPT_INCLUDE_DIRS})
+set(GCRYPT_LIBRARIES ${GCRYPT_LDFLAGS})
+set(GCRYPT_VERSION ${GCRYPT_VERSION})
 
-#reset variables
-set(LIBGCRYPT_LIBRARIES)
-set(LIBGCRYPT_CFLAGS)
+# Provide imported target for Gcrypt
+add_library(Gcrypt::Gcrypt UNKNOWN IMPORTED)
+set_target_properties(Gcrypt::Gcrypt PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${GCRYPT_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES "${GCRYPT_LIBRARIES}"
+)
 
-# if libgcrypt-config has been found
-IF(LIBGCRYPTCONFIG_EXECUTABLE)
+# Find the actual location of the Gcrypt library file
+find_library(GCRYPT_LIBRARY
+        NAMES libgcrypt.so libgcrypt.dylib libgcrypt.a
+        HINTS ${GCRYPT_LIBRARY_DIRS}
+)
 
-  # workaround for MinGW/MSYS
-  # CMake can't starts shell scripts on windows so it need to use sh.exe
-  EXECUTE_PROCESS(COMMAND sh ${LIBGCRYPTCONFIG_EXECUTABLE} --libs RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE LIBGCRYPT_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
-  EXECUTE_PROCESS(COMMAND sh ${LIBGCRYPTCONFIG_EXECUTABLE} --cflags RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE LIBGCRYPT_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
-  EXECUTE_PROCESS(COMMAND sh ${LIBGCRYPTCONFIG_EXECUTABLE} --version RESULT_VARIABLE _return_VALUEVersion OUTPUT_VARIABLE LIBGCRYPT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+# Set the imported location dynamically
+if (GCRYPT_LIBRARY)
+    set_target_properties(Gcrypt::Gcrypt PROPERTIES
+            IMPORTED_LOCATION "${GCRYPT_LIBRARY}"
+    )
+else ()
+    message(FATAL_ERROR "Gcrypt library not found")
+endif ()
 
-  IF(NOT LIBGCRYPT_CFLAGS AND NOT _return_VALUE)
-    SET(LIBGCRYPT_CFLAGS " ")
-  ENDIF(NOT LIBGCRYPT_CFLAGS AND NOT _return_VALUE)
-
-  IF(LIBGCRYPT_LIBRARIES AND LIBGCRYPT_CFLAGS)
-    SET(LIBGCRYPT_FOUND TRUE)
-  ENDIF(LIBGCRYPT_LIBRARIES AND LIBGCRYPT_CFLAGS)
-
-  if(LIBGCRYPT_VERSION VERSION_LESS ${LibGcrypt_FIND_VERSION})
-     message(WARNING "libgcrypt found but version is less than required, Found ${LIBGCRYPT_VERSION} Required ${LibGcrypt_FIND_VERSION}")
-     SET(LIBGCRYPT_FOUND FALSE)
-  endif()
-
-ENDIF(LIBGCRYPTCONFIG_EXECUTABLE)
-
-if (LIBGCRYPT_FOUND)
-   if (NOT LibGcrypt_FIND_QUIETLY)
-      message(STATUS "Found libgcrypt: ${LIBGCRYPT_LIBRARIES}")
-   endif (NOT LibGcrypt_FIND_QUIETLY)
-else (LIBGCRYPT_FOUND)
-   if (LibGcrypt_FIND_REQUIRED)
-      message(WARNING "Could not find libgcrypt libraries")
-   endif (LibGcrypt_FIND_REQUIRED)
-endif (LIBGCRYPT_FOUND)
-
-MARK_AS_ADVANCED(LIBGCRYPT_CFLAGS LIBGCRYPT_LIBRARIES)
+# Print Gcrypt information
+message(STATUS "Found Gcrypt ${GCRYPT_VERSION}")
+message(STATUS "Gcrypt include directories: ${GCRYPT_INCLUDE_DIR}")
+message(STATUS "Gcrypt libraries: ${GCRYPT_LIBRARIES}")
--- CMakeModules/FindReadline.cmake
+++ CMakeModules/FindReadline.cmake
@@ -1,35 +1,48 @@
-# Tries to find the local libreadline installation.
-# Once done the following variables will be defined:
-#
-# READLINE_FOUND        - system has readline
-# READLINE_INCLUDE_DIRS - readline include directories
-# READLINE_LIBRARIES    - libraries need to use readline
-#
-# and the following imported targets
-#
-# Readline::readline
+# A CMake module to find a local installation of the Readline library
 
-find_path(READLINE_INCLUDE_DIR
-  NAMES readline/readline.h
-  HINTS ${READLINE_ROOT})
+# Find Readline library
+# This module sets the following variables:
+#   READLINE_FOUND       - True if Readline is found
+#   READLINE_INCLUDE_DIR - Include directories for Readline
+#   READLINE_LIBRARIES   - Linker flags for Readline
+#   READLINE_VERSION     - Version of Readline
 
+# This module also provides an imported target:
+#   Readline::Readline   - Readline library
+
+# Find the pkg-config package for Readline
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(READLINE REQUIRED readline)
+
+# Set the Readline variables
+set(READLINE_FOUND TRUE)
+set(READLINE_INCLUDE_DIR ${READLINE_INCLUDE_DIRS})
+set(READLINE_LIBRARIES ${READLINE_LDFLAGS})
+set(READLINE_VERSION ${READLINE_VERSION})
+
+# Provide imported target for Readline
+add_library(Readline::Readline UNKNOWN IMPORTED)
+set_target_properties(Readline::Readline PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${READLINE_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES "${READLINE_LIBRARIES}"
+)
+
+# Find the actual location of the Readline library file
 find_library(READLINE_LIBRARY
-  NAMES readline
-  HINTS ${READLINE_ROOT}
-  PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
+        NAMES libreadline.so libreadline.dylib libreadline.a
+        HINTS ${READLINE_LIBRARY_DIRS}
+)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Readline
-  REQUIRED_VARS READLINE_LIBRARY READLINE_INCLUDE_DIR)
+# Set the imported location dynamically
+if (READLINE_LIBRARY)
+    set_target_properties(Readline::Readline PROPERTIES
+            IMPORTED_LOCATION "${READLINE_LIBRARY}"
+    )
+else ()
+    message(FATAL_ERROR "Readline library not found")
+endif ()
 
-mark_as_advanced(READLINE_FOUND READLINE_LIBRARY READLINE_INCLUDE_DIR)
-
-if (READLINE_FOUND AND NOT TARGET Readline::readline)
-  add_library(Readline::readline UNKNOWN IMPORTED)
-  set_target_properties(Readline::readline PROPERTIES
-    IMPORTED_LOCATION "${READLINE_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${READLINE_INCLUDE_DIR}")
-endif()
-
-set(READLINE_INCLUDE_DIRS ${READLINE_INCLUDE_DIR})
-set(READLINE_LIBRARIES ${READLINE_LIBRARY})
+# Print Readline information
+message(STATUS "Found Readline ${READLINE_VERSION}")
+message(STATUS "Readline include directories: ${READLINE_INCLUDE_DIR}")
+message(STATUS "Readline libraries: ${READLINE_LIBRARIES}")
