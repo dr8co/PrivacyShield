df91eaeaf63b3f4a4617c8f59854b7525dc3e164
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-09-24 15:21:10 +0300

    Run the scripts with root access

--- buildscript.sh
+++ buildscript.sh
@@ -3,19 +3,25 @@
 # This script is used to build the project on the Ubuntu Jammy (22.04) distribution.
 # It is not intended to be used on other distributions, and must be run from the project root.
 
+# Root access is required to install the dependencies.
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    exit
+fi
+
 # Install the dependencies.
-wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-sudo add-apt-repository -y "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main"
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
-sudo apt update
-sudo apt install -y cmake ninja-build gcc-13 g++-13 clang-17 lldb-17 lld-17 libc++-17-dev \
+wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+add-apt-repository -y "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main"
+add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
+apt update
+apt install -y cmake ninja-build gcc-13 g++-13 clang-17 lldb-17 lld-17 libc++-17-dev \
     libc++abi-17-dev libomp-17-dev libgcrypt20 openssl libreadline8 libsodium23 libsodium-dev
 
 # Build BLAKE3
 git clone https://github.com/BLAKE3-team/BLAKE3.git
 cd BLAKE3/c || exit
 cmake -B build -DCMAKE_C_COMPILER=clang++-17 -G Ninja
-sudo cmake --build build --config Release --target install -j 4
+cmake --build build --config Release --target install -j 4
 
 # Configure CMake
 cd ../../ || exit
--- install_blake3.sh
+++ install_blake3.sh
@@ -3,10 +3,16 @@
 # This script is used to build and install BLAKE3 on Unix-like systems.
 # Requires CMake, Ninja, and GCC (or a compatible C compiler) to be installed.
 
+# Root access is required to install BLAKE3 to the system.
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    exit
+fi
+
 # Clone the repository
 git clone https://github.com/BLAKE3-team/BLAKE3.git
 
 # Build and install BLAKE3
 cd BLAKE3/c || echo "Failed to find BLAKE3/c directory" && exit
 cmake -B build -DCMAKE_C_COMPILER=gcc -G Ninja
-sudo cmake --build build --config Release --target install -j 4
+cmake --build build --config Release --target install -j 4
