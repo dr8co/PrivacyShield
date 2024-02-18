e8f4a2543543c35ebaae041786a1226f82a40387
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2024-02-18 20:14:16 +0300

    Update build script
    Update the building script to be compatible with C++20 Modules

--- scripts/buildscript.sh
+++ scripts/buildscript.sh
@@ -5,7 +5,6 @@ set -e
 # This script is used to build the project on the Ubuntu Jammy (22.04) distribution.
 # It is not intended to be used on other distributions, and must be run from the project root.
 
-REQUIRED_PACKAGES="ninja-build gcc-13 g++-13 clang-18 lldb-18 lld-18 libc++-18-dev libc++abi-18-dev clang-tools-18 libgcrypt20 openssl libreadline8 libsodium23 libsodium-dev"
 PARALLELISM_LEVEL=4
 
 function check_root() {
@@ -17,7 +16,7 @@ function check_root() {
 }
 
 function check_dependencies() {
-  for cmd in wget add-apt-repository cmake; do
+  for cmd in wget add-apt-repository; do
     if ! command -v $cmd &>/dev/null; then
       echo "$cmd could not be found"
       exit
@@ -30,17 +29,26 @@ function install_dependencies() {
   add-apt-repository -y "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-18 main"
   add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
   apt update
-  apt install -y "$REQUIRED_PACKAGES"
+  apt install -y unzip gcc-13 g++-13 clang-18 lldb-18 lld-18 libc++-18-dev libc++abi-18-dev clang-tools-18 libgcrypt20 openssl libreadline8 libsodium23 libsodium-dev
 
   # Install CMake 3.28.3
   if dpkg -s "cmake" >/dev/null 2>&1; then
     apt remove -y --purge --auto-remove cmake
-    wget -qO- "https://github.com/Kitware/CMake/releases/download/v3.28.3/cmake-3.28.3-linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
   fi
+
+  wget -qO- "https://github.com/Kitware/CMake/releases/download/v3.28.3/cmake-3.28.3-linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
+
+  # Install Ninja 1.11
+  if dpkg -s "ninja-build" >/dev/null 2>&1; then
+    apt remove -y --purge --auto-remove ninja-build
+  fi
+
+  wget -q "https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-linux.zip"
+  unzip ninja-linux.zip -d /usr/local/bin
 }
 
 function build_blake3() {
-  ./install_blake3.sh clang-18
+  ./install-blake3.sh clang-18
 }
 
 function configure_cmake() {
@@ -57,7 +65,9 @@ main() {
   check_dependencies
   cd "${0%/*}" || abort
   install_dependencies
+  echo "Ninja: $(ninja --version), CMake: $(cmake --version)"
   build_blake3
+  cd .. || abort
   configure_cmake
   build_project
 }
