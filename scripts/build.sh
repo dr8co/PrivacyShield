790a30454034b31cc74105cfb2e4c9d38a12bcbd
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2024-05-31 01:59:38 +0300

    Fix Ninja installation

--- scripts/build.sh
+++ scripts/build.sh
@@ -44,7 +44,7 @@ function install_dependencies() {
     apt remove -y --purge --auto-remove ninja-build
   fi
 
-  wget -qO- "https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip" | unzip -d /usr/local/bin
+  wget -qO- "https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip" | funzip > /usr/local/bin/ninja
 }
 
 function build_blake3() {
