cad180a91c3db663920939f588085108f94b6c12
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2024-06-05 21:01:46 +0300

    Configure Qodana code analysis

---
+++ scripts/prepare-qodana.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Run from this directory
+cd "$(dirname "$0")" || (echo "Running from $(pwd)" && exit 1)
+
+# Include the build functions
+. ./build-functions.sh
+
+# Root access is required to install the dependencies.
+check_root
+
+# Check for required commands
+check_dependencies
+
+# Install dependencies
+install_dependencies
+
+echo "Ninja: $(ninja --version), CMake: $(cmake --version)"
+
+# Build and install BLAKE3
+build_blake3
+
+# Configure CMake
+cd .. || abort
+/usr/local/bin/cmake -S . -B build -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++-18 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+
